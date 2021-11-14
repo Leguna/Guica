d12cc48a348bc17640ec86bb3b54e5a75b12e67d
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2021-11-14 19:14:09 +0700

    add sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//API route for register new user
+Route::post('/register', [AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [AuthController::class, 'login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('v1')->apiResources('guild/card/');
 });
-
-
-Route::get('/dashboard', function () {
-    return "Authenticated";
-});
-
-//Route::prefix('v1')->apiResources('guild/card/');
